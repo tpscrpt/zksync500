d194c225780cc36e2b440fd7e741735388e67a5b
Jeremi G <gendronjeremi@gmail.com>
2020-12-03 17:05:50 -0500

    fix/backend/short: create-short not using proper hex string

--- packages/backend/src/functions/short/helpers/create-short.ts
+++ packages/backend/src/functions/short/helpers/create-short.ts
@@ -2,6 +2,6 @@ import crypto from "crypto";
 import { ethers } from "ethers";
 
 export function createShort(): string {
-  const hexString = crypto.randomBytes(16).toString("hex");
+  const hexString = "0x" + crypto.randomBytes(16).toString("hex");
   return ethers.utils.base58.encode(hexString);
 }
