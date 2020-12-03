007d43dd555d3700090ddfdc8364be8e52607e8d
Jeremi G <gendronjeremi@gmail.com>
2020-12-03 17:08:51 -0500

    up/frontend/app.tsx: better app structure (nav bar, router)

--- packages/frontend/src/app.tsx
+++ packages/frontend/src/app.tsx
@@ -3,16 +3,28 @@ import * as ReactDOM from "react-dom";
 import { UrlContext } from "./hooks/url-context";
 import { getUrlParams } from "./hooks/url-context/actions";
 import { PaymentForm } from "./components/payment-form";
+import { BuilderForm } from "./components/builder-form";
+import { Nav } from "./components/nav";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App(): JSX.Element {
   return (
-    <>
-      <h1 style={{ textAlign: "center" }}>zksync500</h1>
-      <br />
-      <UrlContext.Provider value={getUrlParams()}>
-        <PaymentForm />
-      </UrlContext.Provider>
-    </>
+    <Router>
+      <Nav />
+      <Container fluid className="mt-3" style={{ maxWidth: "500px" }}>
+        <Switch>
+          <Route exact path="/create">
+            <BuilderForm />
+          </Route>
+          <Route path="/">
+            <UrlContext.Provider value={getUrlParams()}>
+              <PaymentForm />
+            </UrlContext.Provider>
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   );
 }
 
